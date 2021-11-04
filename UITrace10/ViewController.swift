321080d3e917d3252df7189deabb478ce7420c7e
Nanami Kurata <krtnnm20011031@gmail.com>
2021-11-04 13:23:44 +0900

    first commit

---
+++ UITrace10/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  UITrace10
+//
+//  Created by 倉田七海 on 2021/11/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var firstImage: UIImageView!
+    @IBOutlet weak var expLabel: UILabel!
+    @IBOutlet weak var google: UIImageView!
+    
+    @IBOutlet weak var labelGoogle: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    @IBOutlet weak var emailImage: UIImageView!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var login: UILabel!
+}
+
